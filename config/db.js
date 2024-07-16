3441c2a239389da4ef8c2e8df22a2d0b754cb184
sami081 <makhloufisami0811@gmail.com>
2024-07-16 17:26:12 +0200

    vercel

--- config/db.js
+++ config/db.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
-    mongoose.connect(
-      "mongodb+srv://sami:sami1987@crud.dndehun.mongodb.net/crud"
-    );
+    mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to mongoDB");
   } catch {
     (err) => console.log("Failled to connect", err);
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
+const cors = require('cors');
 const userRoutes = require("./routes/user.routes")
 const app = express();
 
 const connectDB = require("./config/db");
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
