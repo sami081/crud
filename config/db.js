47b7d38e63306871264fc600328c38793d856e54
sami081 <makhloufisami0811@gmail.com>
2024-07-16 17:55:23 +0200

    test

--- config/db.js
+++ config/db.js
@@ -1,14 +1,20 @@
 const mongoose = require("mongoose");
+
 const connectDB = async () => {
   try {
-    mongoose.set("strictQuery", false);
-    mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to mongoDB");
-  } catch {
-    (err) => console.log("Failled to connect", err);
+    await mongoose.connect(
+      process.env.MONGODB_URI,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // d'autres options MongoDB si nécessaires
+      }
+    );
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB:", err.message);
+    process.exit(1); // Arrêter l'application en cas d'échec de connexion
   }
 };
+
 module.exports = connectDB;
