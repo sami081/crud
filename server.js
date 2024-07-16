3051d2fcf7c37b6e86e867ad1cb6f76183d6aa28
sami081 <makhloufisami0811@gmail.com>
2024-07-16 18:43:53 +0200

    modif server

--- server.js
+++ server.js
@@ -13,7 +13,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/user", userRoutes);
 
 //server
+const http = require('http');
 const port = process.env.PORT || 5000; // Défaut à 3000 si en local
-app.listen(port, () => {
-  console.log(`${port}`);
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
