4fdafb566dd50bf41da7bc5676fa1b6d88e7f5ff
sami081 <makhloufisami0811@gmail.com>
2024-07-18 11:34:19 +0200

    add-password

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,10 @@
 const UserModel = require("../models/user.model")
 
 module.exports.signUp = async (req, res)=> {
-const {name, email, birthday, phone, } = req.body
+const {name, email, birthday, phone, password} = req.body
 
 try {
-    const user = await UserModel.create({name, email, birthday, phone,});
+    const user = await UserModel.create({name, email, birthday, phone, password});
     res.status(201).json({user: user._id})
 }
 catch (err) {
--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
   {
@@ -26,7 +26,12 @@ const userSchema = new mongoose.Schema(
       type: String,
       unique: true,
     },
- 
+    password: {
+      type: String,
+      required: true,
+      maxLength: 1024,
+      minLength: 6,
+    },
   },
   {
     timestamps: true,
@@ -35,7 +40,10 @@ const userSchema = new mongoose.Schema(
 
 //
 
-
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  (this.password = await bcrypt.hash(this.password, salt)), next();
+});
 
 const UserModel = mongoose.model("user", userSchema);
 
