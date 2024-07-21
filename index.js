f7b3402a95bf16549574b3453ec8c1991a1623e4
sami081 <makhloufisami0811@gmail.com>
2024-07-22 00:55:57 +0200

    modif cors

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
 const userRoutes = require("./routes/user.routes");
-const {checkUser, requireAuth}= require("./middleware/auth.middleware")
+const { checkUser, requireAuth } = require("./middleware/auth.middleware");
 const app = express();
 require("dotenv").config({ path: "./config/.env" });
 const connectDB = require("./config/db");
 connectDB();
 const corsOptions = {
-  origin :process.env.CLIENT_URL,
-  credentials : true,
-  'allowedHeaders' : ['sessionId', 'Content-type'],
-  'exposedHeaders' : ['sessionId'],
-  'methods' : 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  'preflightContinue' : false
-}
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  allowedHeaders: [
+    "sessionId",
+    "Content-type",
+    "Origin",
+    "Accept",
+    "Authorization",
+    "Token",
+  ],
+  exposedHeaders: ["sessionId"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+};
 
 app.use(cors(corsOptions));
 
@@ -22,13 +29,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
-
 //jwt
-app.get("*", checkUser)
-app.get("/jwtid", requireAuth, (req, res)=> {
-  res.status(200).send(res.locals.user._id)
-})
+app.get("*", checkUser);
+app.get("/jwtid", requireAuth, (req, res) => {
+  res.status(200).send(res.locals.user._id);
+});
 // routes
 
 app.use("/user", userRoutes);
