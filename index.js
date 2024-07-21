4362fafdfce77b20829f73da84c742fadb22b6c8
sami081 <makhloufisami0811@gmail.com>
2024-07-22 01:25:43 +0200

    cors

--- index.js
+++ index.js
@@ -7,22 +7,23 @@ const app = express();
 require("dotenv").config({ path: "./config/.env" });
 const connectDB = require("./config/db");
 connectDB();
-// const allowedOrigins = ['https://crud-front-omega.vercel.app'];
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  allowedHeaders: [
+    "sessionId",
+    "Content-type",
+    "Origin",
+    "Accept",
+    "Authorization",
+    "Token",
+  ],
+  exposedHeaders: ["sessionId"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+};
 
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     // autoriser les requêtes sans origine comme les applications mobiles ou les requêtes curl
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) === -1) {
-//       const msg = 'La politique CORS de ce site ne permet pas l\'accès depuis l\'origine spécifiée.';
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//   },
-//   optionsSuccessStatus: 200
-// };
-
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
